dc0cc6107743e91791fdef4c38477faf33044ab1
nam.kt <nam.khuatthanh@adamosoft.com>
2024-03-22 14:27:43 +0700

    Add initial project files

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  const userAgent = req.headers["user-agent"];
+  if (isBot(userAgent)) {
+    console.log("Request from a bot:", userAgent);
+    // Handle bot request
+    res.send("Hello, bot!");
+  } else {
+    console.log("Request from a regular user:", userAgent);
+    // Handle regular user request
+    res.send("Hello, user!");
+  }
+});
+
+function isBot(userAgent) {
+  // Define a list of known bot user agents
+  const botUserAgents = [
+    "Googlebot",
+    "Bingbot",
+    "YandexBot",
+    "DuckDuckBot",
+    "Baiduspider",
+    "FacebookExternalHit",
+    "Twitterbot",
+    "LinkedInBot",
+    "Slackbot",
+    "TelegramBot",
+    // Add more bot user agents as needed
+  ];
+  // Check if the user agent matches any known bot user agents
+  return botUserAgents.some((botAgent) => userAgent.includes(botAgent));
+}
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ pre-render.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const axios = require("axios");
+const puppeteer = require("puppeteer");
+
+
+const API_URL = 'http://34.125.106.24/';
+
+// Function to fetch website content
+async function fetchContent(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching content:", error);
+    return null;
+  }
+}
+
+// Function to render HTML using Puppeteer
+async function renderHTML(url, filename) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: "networkidle0" });
+    const content = await page.content();
+    await browser.close();
+    fs.writeFileSync(filename, content);
+    console.log(`Rendered ${url} to ${filename}`);
+  } catch (error) {
+    console.error("Error rendering HTML:", error);
+  }
+}
+
+// Define website URLs and filenames for pre-rendering
+const pagesToRender = [
+  { url: `${API_URL}`, filename: "rendered/home.html" },
+  //{ url: `${API_URL}login`, filename: "rendered/login.html" },
+  // Add more pages as needed
+];
+
+// Fetch content and render HTML for each page
+async function preRenderPages() {
+  for (const page of pagesToRender) {
+    const content = await fetchContent(page.url);
+    if (content) {
+      renderHTML(page.url, page.filename);
+    }
+  }
+}
+
+// Start pre-rendering
+preRenderPages();
