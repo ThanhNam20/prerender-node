c5f758de22df88a142c95379647415933e5d4772
nam.kt <nam.khuatthanh@adamosoft.com>
2024-03-29 15:22:54 +0700

    Add fs module import

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const fs = require("fs");
 const { BOT_PATTERNS } = require("./const.js");
 const uaParser = require("ua-parser-js");
 
